895347d10e4b2cc72b89e8c81a80c5dd82d7e680
nuridavid <nurieldavid@gmail.com>
2021-08-11 05:16:29 -0400

    added coin component and added search functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Coin from "./Coin";
+import { filter } from "minimatch";
 const url =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   useEffect(() => {
     axios
       .get(url)
@@ -19,9 +31,25 @@ function App() {
       <div className="coin-search">
         <h1 className="coin-text">Search for a currency</h1>
         <form>
-          <input type="text" placeholder="Search" className="coin-input" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="coin-input"
+            onChange={handleChange}
+          />
         </form>
       </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.market_cap}
+          />
+        );
+      })}
     </div>
   );
 }
